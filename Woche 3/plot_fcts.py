ee319f9b8797b57378dc352c50bda727b1edb21d
janneklink <linkjannek@gmail.com>
2021-05-03 09:06:53 +0200

    Woche 3

---
+++ Woche 3/plot_fcts.py
@@ -0,0 +1,31 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+from matplotlib import cm
+
+def fun(x, y):
+    #return x*y/(x**2+y**2)
+    return x**2-y**2
+    
+
+
+fig = plt.figure()
+ax = plt.axes(projection='3d')
+
+
+x  = y = np.arange(-1, 1, 0.01)
+X, Y = np.meshgrid(x, y)
+Z = np.array(fun(X, Y))
+
+
+surf = ax.plot_surface(X, Y, Z, cmap=cm.coolwarm)
+
+
+ax.set_xlabel('x')
+ax.set_ylabel('y')
+ax.set_zlabel('z')
+
+fig.colorbar(surf, shrink=0.5, aspect=5)
+
+
+plt.show()
